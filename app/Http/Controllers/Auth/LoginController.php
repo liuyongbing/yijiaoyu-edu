35d0e2c39e0e57013a66adb515d850f90a70c921
Gio <liu.gio@lexisnexis.com>
2018-05-23 11:24:42 +0800

    STATIC_VERSIONwq

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,6 +38,7 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         
+        parent::__construct();
         $this->init();
     }
     
