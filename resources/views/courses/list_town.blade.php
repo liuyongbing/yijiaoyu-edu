d60025b4afc4bd6904aa3bbc86aa64e9b5f43811
liuyongbing <54026099@qq.com>
2018-09-12 11:37:15 +0800

    develop_town

--- resources/views/courses/list_town.blade.php
+++ resources/views/courses/list_town.blade.php
@@ -3,29 +3,34 @@
 @section('title', '色带列表')
 
 @section('body')
-<body class="body-bg">
 
-<div class="stage">
-	<div class="center prel">
-		<div class="logo logo-add logo-dm pabs"></div>
-		<div class="stage-list stage-list-add prel">
-        @if (!empty($items))
-            @foreach($items as $key => $item)
-			<a class="a0{{ $key+1 }} @if(($key+1) == count($items)) pabs @endif" href="{{ route('courses.show', ['id' => $item['id']]) }}">
-				<span>
-					<label>{{ $item['title'] }}</label>
-					<img src="{{ $item['image_url'] }}" class="reflect" />
-				</span>
-			</a>
-            @endforeach
-        @else
-            <h1 style="color:#FFF">即将上线</h1>
-        @endif
-		</div>
-	</div>
+<body class="body-bg03">
+<div class="kp">
+    <div class="center prel">
+        <div class="logo pabs"></div>
+    </div>
+
+    <div class="htmleaf-container">
+        <div class="container">
+            <div class="stage-list">
+            @if (!empty($items))
+                @foreach($items as $key => $item)
+                <a style="padding-top: {{ 120-20*$key }}px" href="{{ route('courses.show', ['id' => $item['id']]) }}">
+                    <span>{{ $item['title'] }}</span>
+                    <img src="{{ $item['image_url'] }}" />
+                </a>
+                @endforeach
+            @else
+                <h1 style="color:#FFF">即将上线</h1>
+            @endif
+            </div>
+        </div>
+    </div>
 </div>
+
 @include('layouts.logout')
 </body>
+
 @endsection
 
 @section('script')
--- resources/views/grades/list_town.blade.php
+++ resources/views/grades/list_town.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('layouts.main_town')
 
 @section('style')
 <link rel="stylesheet" type="text/css" href="{{ asset(elixir('town/css/jquery.vm-carousel.css')) }}{{ $STATIC_VERSION }}">
@@ -7,29 +7,30 @@
 @section('title', '卡牌列表')
 
 @section('body')
+
 <body class="body-bg03">
-	<div class="kp">
-		<div class="center prel">
-			<div class="logo pabs"></div>
-		</div>
-		
-		<div class="htmleaf-container">
-            <div class="container">
-                <ul class="vmcarousel-centered vmc-centered">
-                    @foreach($items as $item)
-                    <li>
-                        <label>{{ $item['title'] }}</label>
-                        <a href="{{ route('courses.index', ['grade_id' => $item['id']]) }}">
-                            <img class="reflect" src="{{ $item['image_url'] }}" />
-                        </a>
-                    </li>
-                    @endforeach
-                </ul>
-            </div>
+<div class="kp">
+    <div class="center prel">
+        <div class="logo pabs"></div>
+    </div>
+
+    <div class="htmleaf-container">
+        <div class="container">
+            <ul class="vmcarousel-centered vmc-centered">
+            @foreach($items as $item)
+                <li>
+                    <label>{{ $item['title'] }}</label>
+                    <a href="{{ route('courses.index', ['grade_id' => $item['id']]) }}">
+                        <img class="reflect" src="{{ $item['image_url'] }}" />
+                    </a>
+                </li>
+            @endforeach
+            </ul>
         </div>
     </div>
-    
-    @include('layouts.logout')
+</div>
+
+@include('layouts.logout')
 </body>
 @endsection
 
@@ -38,13 +39,13 @@
 <script type="text/javascript" src="{{ asset(elixir('town/js/reflection.js')) }}{{ $STATIC_VERSION }}"></script>
 <script src="{{ asset(elixir('town/js/jquery.vm-carousel.js')) }}{{ $STATIC_VERSION }}"></script>
 <script type="text/javascript">
-	jQuery(function($){
-            $('.vmcarousel-centered').vmcarousel({
-                centered: true,
-                start_item: 2,
-                autoplay: false,
-                infinite: true
-            });
+    jQuery(function($){
+        $('.vmcarousel-centered').vmcarousel({
+            centered: true,
+            start_item: 1,
+            autoplay: false,
+            infinite: false
         });
+    });
 </script>
 @endsection
